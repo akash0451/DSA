f20a3c70756377b1e0fe610a984734172e9d74d0
Akash Gupta <akashakg05@gmail.com>
2026-08-24 11:49:34 +0530

    Time: 0 ms (100%), Space: 16.5 MB (44.53%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        vector<int>upper(26,0);
+        vector<int>lower(26,0);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' )
+            {
+                upper[s[i]-'A']++;
+                s[i]='#';
+            }
+        
+            else if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' )
+            {
+                lower[s[i]-'a']++;
+                s[i]='#';
+            }
+        }
+         
+        string ans="";
+        for(int i=0;i<26;i++){
+            char c='A'+i;
+            while(upper[i]){
+                ans+=c;
+                upper[i]--;
+            }
+        }
+        for(int i=0;i<26;i++){
+            char c='a'+i;
+            while(lower[i]){
+                ans+=c;
+                lower[i]--;
+                
+            }
+        }
+        int i=0,j=0;
+        while(j<ans.size()){
+            if(s[i]=='#'){
+                s[i]=ans[j];
+                j++;
+            }
+            i++;
+        }
+         return s;
+    }
+};
