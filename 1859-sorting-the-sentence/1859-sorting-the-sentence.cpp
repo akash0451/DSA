69638138d2d8dd7ebafdb9e67219c45dc27412b2
Akash Gupta <akashakg05@gmail.com>
2026-08-29 10:20:06 +0530

    Time: 0 ms (100%), Space: 8.6 MB (41.54%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        string temp;
+        vector<string>ans(10);
+        int count=0,i=0;
+        while(i<  s.size()){
+            if(s[i]==' '){
+                int pos=temp[temp.size()-1]-'0';
+                temp.pop_back();
+                ans[pos]=temp;
+                temp.clear();
+                count++;
+                
+            }
+            else{
+            temp+=s[i];
+            }
+            i++;
+        }
+    
+                int pos=temp[temp.size()-1]-'0';
+                temp.pop_back();
+                ans[pos]=temp;
+                temp.clear();
+                count++;
+                
+            
+        for(int i=1;i<=count;i++){
+            temp+=ans[i];
+            temp+=' ';
+        }
+        
+        
+        temp.pop_back();
+        return temp;
+ 
+    }
+};
