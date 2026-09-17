74bdf5b872ed93120b4244e9fe4698d3446d11ba
Akash Gupta <akashakg05@gmail.com>
2026-09-17 18:53:50 +0530

    Time: 0 ms (100%), Space: 8 MB (71.96%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        vector<int>p(n+1,0);
-        vector<int>s(n+1,0);
-        int i=1,j=n;
-        int sum=0,sum2=0;
-        while(i<=n)
-        {
-            sum+=i;
-            p[i]=sum;
-            i++;
+        // vector<int>p(n+1,0);
+        // vector<int>s(n+1,0);
+        // int i=1,j=n;
+        // int sum=0,sum2=0;
+        // while(i<=n)
+        // {
+        //     sum+=i;
+        //     p[i]=sum;
+        //     i++;
+        // }
+        // while(j>0)
+        // {
+        //    sum2+=j;
+        //    s[j]=sum2;
+        //    j--;
+        // }
+        // for(int i=1;i<p.size();i++)
+        // {
+        //     if(p[i]==s[i])
+        //     return i;
+        // }
+         int prefixSum = 1;
+        int suffixSum = n;
+
+        int i = 1;
+        int j = n;
+
+        while (i < j) {
+            if (prefixSum < suffixSum) {
+                i++;
+                prefixSum += i;
+            } else {
+                j--;
+                suffixSum += j;
+            }
         }
-        while(j>0)
-        {
-           sum2+=j;
-           s[j]=sum2;
-           j--;
-        }
-        for(int i=1;i<p.size();i++)
-        {
-            if(p[i]==s[i])
-            return i;
-        }
+
+        return prefixSum == suffixSum ? i : -1;
 
     return -1;
     }
