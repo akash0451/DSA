3a92d4e918b103235da66a31c3cedda979d8ed62
Akash Gupta <akashakg05@gmail.com>
2026-09-17 18:52:47 +0530

    Time: 2 ms (21.1%), Space: 11.7 MB (5.17%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        vector<int>p(n+1,0);
+        vector<int>s(n+1,0);
+        int i=1,j=n;
+        int sum=0,sum2=0;
+        while(i<=n)
+        {
+            sum+=i;
+            p[i]=sum;
+            i++;
+        }
+        while(j>0)
+        {
+           sum2+=j;
+           s[j]=sum2;
+           j--;
+        }
+        for(int i=1;i<p.size();i++)
+        {
+            if(p[i]==s[i])
+            return i;
+        }
+
+    return -1;
+    }
+};
